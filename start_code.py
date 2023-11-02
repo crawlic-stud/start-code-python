51a3e77379fa9bc78c6a041bc682b4b77f03368b
crawlic-stud <nikitosik0726@gmail.com>
2023-11-02 16:01:23 +0300

    small changes

--- start_code.py
+++ start_code.py
@@ -11,39 +11,46 @@ proj_path.mkdir(exist_ok=True)
 src_path = proj_path / "src"
 src_path.mkdir(exist_ok=True)
 
-(src_path / "main.py").write_text('''
+(src_path / "main.py").write_text(
+    """
 
 if __name__ == "__main__":
     ...
-''')
+"""
+)
 
 (proj_path / "requirements.txt").touch()
-(proj_path / ".gitignore").write_text('''venv
+(proj_path / ".gitignore").write_text(
+    """venv
 .env
 __pycache__
-''')
+.vscode
+"""
+)
 (proj_path / ".env").touch()
 (proj_path / ".env.example").touch()
 
-(proj_path / "Dockerfile").write_text("""FROM python:3.10.8
-
-ADD src/main.py .
+(proj_path / "Dockerfile").write_text(
+    """FROM python:3.10.8
 
 COPY requirements.txt /tmp/requirements.txt
 COPY . .
 
 RUN pip install --no-cache-dir -r /tmp/requirements.txt
 
-CMD ["python", "src/main.py"]""")
+CMD ["python", "src/main.py"]"""
+)
 
-(proj_path / "docker-compose.yml").write_text("""version: "3"
+(proj_path / "docker-compose.yml").write_text(
+    f"""version: "3"
 services:
   prod:
     build:
       context: .
       dockerfile: ./Dockerfile
-    container_name: container-name
-""")
+    container_name: {proj_name}
+"""
+)
 
 (proj_path / "README.md").write_text(f"# {proj_name}")
 
